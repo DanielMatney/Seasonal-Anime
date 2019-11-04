28c3f0926a2925eafca7730854eb33bf726f966c
Daniel Matney <danieljmatney@gmail.com>
2019-11-04 04:39:44 +0000

    update

--- lib/seasonal-anime.rb
+++ lib/seasonal-anime.rb
@@ -1,6 +1,6 @@
-require_relative "./Seasonal_Anime/cli.rb"
-require_relative "./Seasonal_Anime/anime.rb"
-require_relative "./Seasonal_Anime/scraper.rb"
+require_relative './Seasonal_Anime/cli.rb'
+require_relative './Seasonal_Anime/anime.rb'
+require_relative './Seasonal_Anime/scraper.rb'
 require 'nokogiri'
 require 'open-uri'
 puts "hi"
