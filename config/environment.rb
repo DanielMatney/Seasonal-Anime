04b7e21fc6b1cbda5a281511ef6c2421ec6e1fa8
Daniel Matney <danieljmatney@gmail.com>
2019-11-04 03:46:04 +0000

    update

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,6 @@
-require 'bundler'
-Bundler.require
-require_all '../lib'
+require_relative "../lib/Seasonal-Anime/anime.rb"
+require_relative "../lib/Seasonal-Anime/cli.rb"
+require_relative "../lib/Seasonal-Anime/scraper.rb"
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
